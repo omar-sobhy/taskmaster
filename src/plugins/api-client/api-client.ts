75b344525b1fe098a646265e13ac2dae0e11a086
Omar Sobhy <omar.ahmed.sobhy.96@gmail.com>
2022-03-23 21:54:30 +0400

    Update basePath in API client

--- src/plugins/api-client/api-client.ts
+++ src/plugins/api-client/api-client.ts
@@ -5,11 +5,15 @@ import TaskmasterClient from 'taskmaster-client';
 const Plugin = {
   install(Vue: VueConstructor, options: any): void {
     console.log(options);
-    // eslint-disable-next-line no-restricted-globals
-    const basePath = location.host.startsWith('localhost')
-      ? 'http://localhost:3000'
-      : 'http://omarsobhy.dev:3000';
+    const baseProtocol = window.location.protocol;
 
+    const baseHost = window.location.host.startsWith('localhost')
+      ? 'localhost'
+      : 'omarsobhy.dev';
+
+    const basePort = baseProtocol.startsWith('https') ? 3001 : 3000;
+
+    const basePath = `${baseProtocol}//${baseHost}:${basePort}`;
     console.log('Base path', basePath);
     // eslint-disable-next-line no-param-reassign
     Vue.$apiClient = new TaskmasterClient(basePath, options);
